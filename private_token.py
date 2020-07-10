5be67037d75ec7b64e9e2430fbe67bd77ffe37f5
oniewankenobi <clarissa.onie@gmail.com>
2020-07-10 22:28:47 +1000

    finished setup

---
+++ private_token.py
@@ -0,0 +1,6 @@
+# add spotify user id and ouath token
+
+# login to spotify > account overview > copy "username"
+spotify_user_id = ""
+# visit url (https://developer.spotify.com/console/post-playlists/) > "get token"
+spotify_token = ""
